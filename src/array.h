a4c7e1c5bda2ef2d5b0ec1630bf6d8d928906093
pbandj24 <2015376@sluh.org>
2015-02-18 22:51:33 -0600

    Create array.h

---
+++ src/array.h
@@ -0,0 +1,23 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+typedef struct 
+{
+	int alloc,size;
+	void **buffer;
+} array;
+
+array *array_create(int size);
+void array_empty(array *array);
+void array_destroy(array *array);
+void array_append(array *array, void *data);
+void array_push(array *array, void *data);
+void *array_peekLast(array *array);
+void *array_peek(array *array);
+void array_insert(array *array, int index, void *data);
+void *array_remove(array *array, int index);
+void *array_get(const array *array, int index);
+void *array_set(array *array, int index, void *data);
+void array_set_size(array *array, int size);
+int array_get_size(const array *array);
+#endif 
