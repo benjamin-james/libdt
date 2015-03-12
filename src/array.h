b97e3e7867521170dc506603c70d7179a1752f6e
pbandj24 <2015376@sluh.org>
2015-03-11 22:51:07 -0500

    Updated and cleaned array.h

--- src/array.h
+++ src/array.h
@@ -1,27 +1,29 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
-typedef struct 
-{
+struct array {
 	int alloc,size;
 	void **buffer;
-} array;
+};
 
-void array_shuffle(array *a);
-void array_sort(array *a, int (*cmp)(void *, void *));
-array *array_create(int size);
-array *array_copy(array *a);
-void array_empty(array *a);
-void array_destroy(array *a);
-void array_append(array *a, void *data);
-void array_push(array *a, void *data);
-void *array_pop(array *a);
-void *array_peekLast(array *a);
-void *array_peek(array *a);
-void array_insert(array *a, int index, void *data);
-void *array_remove(array *a, int index);
-void *array_get(const array *a, int index);
-void *array_set(array *a, int index, void *data);
-void array_set_size(array *a, int size);
-int array_get_size(const array *a);
-#endif 
+struct array *array_create(int size);
+void array_destroy(struct array *a);
+
+void *array_get(const struct array *a, int index);
+void *array_set(struct array *a, int index, void *data);
+void array_insert(struct array *a, int index, void *data);
+void *array_remove(struct array *a, int index);
+
+void array_append(struct array *a, void *data);
+void array_push(struct array *a, void *data);
+void *array_pop(struct array *a);
+
+struct array *array_copy(struct array *a);
+void array_empty(struct array *a);
+void array_set_size(struct array *a, int size);
+int array_get_size(const struct array *a);
+
+void array_shuffle(struct array *a);
+void array_sort(struct array *a, int (*cmp)(void *, void *));
+
+#endif
