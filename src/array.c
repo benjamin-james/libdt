811e90e07a11aaa82b6dc4f62be9fa8d7165f3bf
pbandj24 <2015376@sluh.org>
2015-03-11 22:49:56 -0500

    Updated and cleaned up array.c

--- src/array.c
+++ src/array.c
@@ -5,60 +5,16 @@
 
 #define START_SIZE 4
 
-void array_shuffle(array *a)
+struct array *array_create(int size)
 {
-	int i,r;
-	void *tmp;
-	for(i = a->size-1; i >= 0; i--)
-	{
-		r = rand() % i;
-		if(a->buffer[i] && a->buffer[r])
-		{
-			tmp = a->buffer[i];
-			a->buffer[i] = a->buffer[r];
-			a->buffer[r] = tmp;
-		}
-	}
-}
-array *array_copy(array *a)
-{
-	array *b = array_create(a->size);
-	int i;
-	for(i = 0; i < a->size; i++)
-	{
-		b->buffer[i] = a->buffer[i];
-	}
-	b->size = a->size;
-	return b;
-}
-void msort(array *a, int begin, int end, int (*cmp)(void *, void *), array *b)
-{
-	if(end <= begin) return;
-	int i,j,k,middle = begin + (end - begin) / 2;
-	msort(a,begin,middle,cmp,b);
-	msort(a,middle+1,end,cmp,b);
-	for(i = begin; i <= end; i++) b->buffer[i] = a->buffer[i];
-	for(i = begin, j = middle+1, k = begin; i <= middle && j <= end; k++)
-	{
-		if(cmp(b->buffer[i],b->buffer[j]) <= 0) a->buffer[k] = b->buffer[i++];
-		else a->buffer[k] = b->buffer[j++];
-	}
-	for(;i <= middle; i++,k++) a->buffer[k] = b->buffer[i];
-}
-void array_sort(array *a, int (*cmp)(void *, void *))
-{
-	array *b = array_copy(a);
-	msort(a,0,array_get_size(a)-1,cmp,b);
-	array_destroy(b);
-}
-array *array_create(int size)
-{
-	array *a = malloc(sizeof(array));
-	if(a == NULL || size <= 0) return NULL;
+	if (size <= 0)
+		return NULL;
+	struct array *a = malloc(sizeof(struct array));
+	if (a == NULL)
+		return NULL;
 	a->buffer = malloc(size * sizeof(void*));
 	a->size = 0;
-	if(a->buffer == NULL)
-	{
+	if (a->buffer == NULL) {
 		free(a);
 		a = NULL;
 	}
@@ -66,171 +22,199 @@ array *array_create(int size)
 	return a;
 }
 
-void array_empty(array *a)
+void array_empty(struct array *a)
 {
 	a->alloc = 0;
 }
 
-void array_destroy(array *a)
+void array_destroy(struct array *a)
 {
-	if(!a) return;
-	if(a->buffer) free(a->buffer);
+	if (!a) 
+		return;
+	if (a->buffer)
+		free(a->buffer);
 	free(a);
 }
 
-void array_append(array *a, void *data)
+void array_append(struct array *a, void *data)
 {
-	if(a->size == a->alloc) 
-	{
-		if(a->buffer != NULL) 
-		{
+	if (a->size == a->alloc) {
+		if (a->buffer != NULL) {
 			a->alloc *= 2;
 			a->buffer = realloc(a->buffer, a->alloc * sizeof(void*));
-		}
-		else 
-		{
+		} else {
 			a->buffer = malloc(START_SIZE * sizeof(void*));
 			a->alloc = START_SIZE;
 		}
 	}
-	if(a->buffer != NULL) 
-	{
+	if (a->buffer != NULL) {
 		a->buffer[a->size] = data;
 		a->size++;
 	}
 }
 
-void array_push(array *a, void * data)
+void array_push(struct array *a, void * data)
 {
 	int i;
-	if(a->size == a->alloc) 
-	{
-		if(a->buffer != NULL) 
-		{
+	if (a->size == a->alloc) {
+		if (a->buffer != NULL) {
 			a->alloc *= 2;
 			void **temp = malloc(a->alloc * sizeof(void*));
-			if(temp) 
-			{
-				for (i = 0; i < a->size; i++) temp[i + 1] = a->buffer[i];
+			if (temp) {
+				for (i = 0; i < a->size; i++) 
+					temp[i + 1] = a->buffer[i];
 				free(a->buffer);
 				a->buffer = temp;
 			}
-		}
-		else 
-		{
+		} else {
 			a->buffer = malloc(START_SIZE * sizeof(void*));
-			if(a->buffer) a->alloc = START_SIZE;
+			if (a->buffer)
+				a->alloc = START_SIZE;
+		}
+	} else {
+		for (i = a->size; i > 0; i--) {
+			a->buffer[i] = a->buffer[i - 1];
 		}
 	}
-	else for(i = a->size; i > 0; i--) a->buffer[i] = a->buffer[i - 1];
-	if(a->buffer != NULL) 
-	{
+	if (a->buffer != NULL) {
 		a->buffer[0] = data;
 		a->size++;
 	}
 }
 
-void *array_peekLast(array *a)
+void array_insert(struct array *a, int pos, void *data)
 {
-	void *data = NULL;
-	if(a->size > 0) 
-	{
-		data = a->buffer[a->size - 1];
-		a->size--;
-	}
-	return data;
-}
-
-void *array_peek(array *a)
-{
-	void *data = NULL;
-	if(a->size > 0) 
-	{
+	if (pos == 0) {
+		array_push(a, data);
+	} else if (pos == a->size) {
+		array_append(a, data);
+	} else if (pos < a->size) {
 		int i;
-		data = a->buffer[0];
-		for (i = 1; i < a->size; i++) a->buffer[i - 1] = a->buffer[i];
-		a->size--;
-	}
-	return data;
-}
-
-void array_insert(array *a, int pos, void *data)
-{
-	if(pos == 0) array_push(a, data);
-	else if(pos == a->size) array_append(a, data);
-	else if(pos < a->size) 
-	{
-		int i;
-		if (a->size == a->alloc) 
-		{
+		if (a->size == a->alloc) {
 			a->alloc *= 2;
 			void **temp = malloc(a->alloc * sizeof(void*));
-			if(temp) 
-			{
+			if (temp) {
 				memcpy(temp, a->buffer, pos * sizeof(void*));
 				memcpy(temp + pos + 1, a->buffer + pos, (a->size - pos) * sizeof(void*));
 				free(a->buffer);
 				a->buffer = temp;
 			}
+		} else {
+			for (i = a->size - 1; i >= pos; i--) {
+				a->buffer[i + 1] = a->buffer[i];
+			}
 		}
-		else for (i = a->size - 1; i >= pos; i--) a->buffer[i + 1] = a->buffer[i];
 		a->buffer[pos] = data;
 		a->size++;
 	}
 }
 
-void *array_remove(array *a, int index)
+void *array_remove(struct array *a, int index)
 {
 	void *data;
-	if (a->size < index + 1) data = NULL;
-	else if (index == 0) data = array_peek(a);
-	else if (index == a->size - 1) data = array_peekLast(a);
-	else 
-	{
-		int i;
+	int i;
+	if (a->size < index + 1) {
+		data = NULL;
+	} else if (index == 0) {
+		data = a->buffer[0];
+		for (i = 1; i < a->size; i++)
+			a->buffer[i - 1] = a->buffer[i];
+		a->size--;
+	} else if (index == a->size - 1) {
 		data = a->buffer[index];
-		for (i = index; i < a->size - 1; i++) a->buffer[i] = a->buffer[i + 1];
+		a->size--;
+	} else {
+		data = a->buffer[index];
+		for (i = index; i < a->size - 1; i++)
+			a->buffer[i] = a->buffer[i + 1];
 		a->size--;
 	}
 	return data;
 }
 
-void *array_get(const array *a, int pos)
+void *array_get(const struct array *a, int pos)
 {
 	void *data = NULL;
-	if (pos < a->size) data = a->buffer[pos];
+	if (pos < a->size)
+		data = a->buffer[pos];
 	return data;
 }
 
-void *array_set(array *a, int pos, void *data)
+void *array_set(struct array *a, int pos, void *data)
 {
-	void *temp = NULL;
-	if (pos == a->size) array_append(a, data);
-	else if (pos < a->size) 
-	{
-		temp = a->buffer[pos];
+	if (pos == a->size) {
+		array_append(a, data);
+		return NULL;
+	} else if (pos < a->size) {
+		void *temp = a->buffer[pos];
 		a->buffer[pos] = data;
+		return temp;
 	}
-	return temp;
+	return NULL;
 }
 
-void array_set_size(array *a, int size)
+void array_set_size(struct array *a, int size)
 {
 	a->buffer = realloc(a->buffer, size);
-	if (a->buffer) 
-	{
+	if (a->buffer)  {
 		a->alloc = size;
-		if (a->alloc < a->size) a->size = a->alloc;
-	}
-	else 
-	{
+		if (a->alloc < a->size) 
+			a->size = a->alloc;
+	} else {
 		a->alloc = 0;
 		a->size = 0;
 	}
 }
 
-int array_get_size(const array *a)
+int array_get_size(const struct array *a)
 {
-	if(!a) return 0;
+	if (!a) 
+		return 0;
 	return a->size;
+}
+void array_shuffle(struct array *a)
+{
+	int i,r;
+	void *tmp;
+	for (i = a->size-1; i >= 0; i--) {
+		r = rand() % i;
+		if (a->buffer[i] && a->buffer[r]) {
+			tmp = a->buffer[i];
+			a->buffer[i] = a->buffer[r];
+			a->buffer[r] = tmp;
+		}
+	}
+}
+struct array *array_copy(struct array *a)
+{
+	struct array *b = array_create(a->size);
+	int i;
+	for (i = 0; i < a->size; i++)
+		b->buffer[i] = a->buffer[i];
+	b->size = a->size;
+	return b;
+}
+void msort(struct array *a, int begin, int end, int (*cmp)(void *, void *), struct array *b)
+{
+	if (end <= begin) 
+		return;
+	int i,j,k,middle = begin + (end - begin) / 2;
+	msort(a,begin,middle,cmp,b);
+	msort(a,middle+1,end,cmp,b);
+	for (i = begin; i <= end; i++) 
+		b->buffer[i] = a->buffer[i];
+	for (i = begin, j = middle+1, k = begin; i <= middle && j <= end; k++) {
+		if (cmp(b->buffer[i],b->buffer[j]) <= 0) 
+			a->buffer[k] = b->buffer[i++];
+		else 
+			a->buffer[k] = b->buffer[j++];
+	}
+	for (;i <= middle; i++,k++) 
+		a->buffer[k] = b->buffer[i];
+}
+void array_sort(struct array *a, int (*cmp)(void *, void *))
+{
+	struct array *b = array_copy(a);
+	msort(a,0,array_get_size(a)-1,cmp,b);
+	array_destroy(b);
 }
