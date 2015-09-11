ea9203ce120638e6f646072a1dc5946b5b22d011
Benjamin James <benjamin-james@utulsa.edu>
2015-09-11 15:22:02 -0500

    obvec

---
+++ src/obvec.c
@@ -0,0 +1,25 @@
+#include "obvec.h"
+
+#ifdef HAS_CONFIG_H
+#include "config.h"
+#endif
+
+int obvec_grow_(struct obstack *ob_stack, void **data, size_t data_size, size_t *obvec_size, size_t *obvec_alloc)
+{
+	return 0;
+}
+
+int obvec_set_(struct obstack *ob_stack, void **data, size_t data_size, size_t *obvec_size, size_t *obvec_alloc, size_t index)
+{
+	return 0;
+}
+
+int obvec_insert_(struct obstack *ob_stack, void **data, size_t data_size, size_t *obvec_size, size_t *obvec_alloc, size_t index)
+{
+	return 0;
+}
+
+int obvec_remove_(struct obstack *ob_stack, void **data, size_t data_size, size_t *obvec_size, size_t *obvec_alloc, size_t index)
+{
+	return 0;
+}
